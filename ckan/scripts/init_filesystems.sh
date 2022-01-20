304d5a99d571aa30489c76b4dec597447fe9560f
Toni Hämeenniemi <toni.hameenniemi@wirokit.com>
2022-01-20 16:44:19 +0200

    fix: remove EFS migration procedure from init_filesystems.sh

--- ckan/scripts/init_filesystems.sh
+++ ckan/scripts/init_filesystems.sh
@@ -7,18 +7,6 @@ if [[ "${DEV_MODE}" == "true" ]]; then
   sudo -E rsync -au --delete ${EXT_DIR}/opendata-assets/resources/ ${WWW_DIR}/resources
   sudo -E chown -R ckan:ckan ${WWW_DIR}/resources
 else
-  # migrate data if migration fs is mounted to known path
-  # NOTE: this is a bad idea, because EFS->EFS rsync perf is so bad!
-  #       it's left here as an example, can be deleted later...
-  if [[ -d "/mnt/ytp_files/ckan" ]]; then
-    echo "init_filesystems - migrating data from '/mnt/ytp_files/ckan' to '${DATA_DIR}' ..."
-    rsync -au --chown=ckan:ckan /mnt/ytp_files/ckan/ ${DATA_DIR}
-    echo "printing migration source contents:"
-    ls -lah /mnt/ytp_files/ckan
-    echo "printing migration destination contents:"
-    ls -lah ${DATA_DIR}
-  fi
-
   # init mounted filesystems if not done or version updated (ECS Fargate EFS limitation forces this approach)
   if [[ "$(cat ${DATA_DIR}/.fs-done)" != "$CKAN_IMAGE_TAG" ]]; then
     echo "init_filesystems - initializing '${DATA_DIR}' & '${WWW_DIR}/resources' ..."
